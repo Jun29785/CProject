023cf3d85893dc67103cc06294da0679604ecb3b
Seungwon <jswa7308@naver.com>
2021-06-21 20:13:59 +0900

    [Delete] function

--- Project1/function.h
+++ Project1/function.h
@@ -8,12 +8,6 @@ int minigamecount1;
 int minigamecount2;
 int minigamecount3;
 
-int BlockNum;
-typedef struct Object {
-	char* image;
-	int X, Y;
-	int height, width;
-}Object;
 
 void gotoxy(int x, int y);
 void HideCursor();
@@ -31,9 +25,5 @@ void Coin(); // 동전 앞뒤
 void RSP(); // 가위바위보 
 int DicegameDraw();
 void DicemainDraw();
-void CreateBlock();
-void PrintBlock();
-void MoveBlock();
-void AvoidBlock();
 int hocDraw();
 void hocmainDraw();
